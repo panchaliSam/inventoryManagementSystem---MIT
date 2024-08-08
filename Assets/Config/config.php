fdd1006195bb0861b415eaaca9d01ea18deffeb0
panchaliSam <panchalisam20030117@gmail.com>
2024-08-08 22:42:55 +0530

    Database config

---
+++ Assets/Config/config.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = " ";
+    $dbname = "InventoryMgt";
+
+    //Create Connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //Check Connection
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+?>
